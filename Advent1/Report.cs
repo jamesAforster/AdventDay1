8fdf631f3b87394bbe3bc76ea006a81f66dcd78e
James Forster <james.forster@justeattakeaway.com>
2020-12-01 18:53:42 +0000

    first commit

---
+++ Advent1/Report.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Advent1
+{
+    public class Report
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int findSum(int[] input)
+        {
+            int totalNumber = 0;
+            foreach (int number in input)
+            {
+                foreach (int otherNumber in input)
+                {
+                    if (number + otherNumber == 2020)
+                    {
+                        totalNumber = number * otherNumber;
+                    }
+                }
+            }
+            return totalNumber;
+        }
+    }
+}
